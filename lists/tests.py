8a3d018a3bbf8abcf7b7c775527456c9fd4a60b4
shufengmuyu <shufengmuyu@qq.com>
2020-04-21 13:23:48 +0800

    Not correct list item

--- lists/tests.py
+++ lists/tests.py
@@ -6,11 +6,12 @@ from lists.views import home_page # (2)
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/') # (1)
-        self.assertEqual(found.func, home_page) # (1)
-
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 # Create your tests here.
