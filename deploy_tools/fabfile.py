cbd7ce67d1f4a617f790bf1b7ab083b7c70f888f
shufengmuyu <shufengmuyu@qq.com>
2020-06-21 17:51:43 +0800

    Add a fabfile for automated deploy

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL=https://github.com/ShuFengMuYu/TDD.git
+REPO_URL='https://github.com/ShuFengMuYu/TDD.git'
 
 def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'
